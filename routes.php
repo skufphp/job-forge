4628be67dd4a6aabc91e67c14b93c4d86be50a53
Vladimir Matkovskii <vlavlamat@icloud.com>
2026-01-18 15:28:55 -0400

    Add dynamic listing details page and update query method for parameterized queries
    - Extended `Database::query` method to support parameterized queries with bound values.
    - Added new controller and view for displaying individual listing details.
    - Updated links in `home.view.php` and `listings/index.view.php` to use query string for listing ID.
    - Modified routing in `routes.php` to include the new listing details endpoint.

--- routes.php
+++ routes.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
-$router->get('404', 'controllers/error/404.php');
+$router->get('/listing', 'controllers/listings/show.php');
